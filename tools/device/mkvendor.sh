ce7288bd905921e2a27e44c6dc9d823e568abd19
Koushik Dutta <koushd@gmail.com>
2012-04-11 10:11:52 -0700

    add pac.mk.template file for mkvendor.sh
    Change-Id: Iba150a5350a7a7dbc9e3444d71d4ff293d51f3d7
    
    fix template for mkvendor.sh
    
    Change-Id: I2c78ed013053efa0c399e022502fd9ec4a1de8e6
    
    mkvendor.sh: force armeabi-v7a
    
    Change-Id: I9f29eec73a13144b47ff7b1db1d59cb1e5a4ef64
    
    mkvendor.sh: typo
    
    Change-Id: I8cc2303fde7c815384e09f65b6d4a1116ac29ee4
    
    mkvendor.sh: more fixes
    
    Change-Id: I0c641d42a96e44bb9170475d5de21cb8644e4d3d
    
    BoardConfig.mk.template: Add TARGET_CPU_VARIANT
    
    Change-Id: I8cd74a794e0d1a6818b5479cf7f5264e87d565f3
    
    BoardConfig.mk.template: Add TARGET_CPU_SMP
    
    This one is easy forget if not in the template
    
    Change-Id: If65e557a661ecc81c6bfc434525b3c9937ff988e

--- tools/device/mkvendor.sh
+++ tools/device/mkvendor.sh
@@ -110,6 +110,6 @@ popd
 
 echo Done!
 echo Use the following command to set up your build environment:
-echo '  'lunch cm_$DEVICE-eng
+echo '  'lunch pac_$DEVICE-eng
 echo And use the follwowing command to build a recovery:
-echo '  '. build/tools/device/makerecoveries.sh cm_$DEVICE-eng
+echo '  '. build/tools/device/makerecoveries.sh pac_$DEVICE-eng

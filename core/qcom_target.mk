c813d3f069e0eec984cd4cae3443b904163157e3
maxwen <max.weninger@gmail.com>
2015-10-22 00:48:40 +0200

    build: CAF bringup
    Change-Id: Ia01e90106165bfa52251dfc3c27603689b33b7aa

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -7,18 +7,11 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     TARGET_GLOBAL_CPPFLAGS += -DQCOM_HARDWARE
 
     ifeq ($(TARGET_USES_QCOM_BSP),true)
-        TARGET_GLOBAL_CFLAGS += -DQCOM_BSP
-        TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP
+        TARGET_GLOBAL_CFLAGS += -DQCOM_BSP -DQTI_BSP
+        TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP -DQTI_BSP
     endif
 
     # Tell HALs that we're compiling an AOSP build with an in-line kernel
     TARGET_COMPILE_WITH_MSM_KERNEL := true
 
-    # Enable DirectTrack for legacy targets
-    ifneq ($(filter caf bfam,$(TARGET_QCOM_AUDIO_VARIANT)),)
-        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
-            TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
-        endif
-    endif
 endif

gimmeitorilltell/platform_build

Add ro.pac.device to build.prop
Change-Id: If08cc13c0f0e92e0dd262a2f71610070a54aaead

tools/buildinfo.sh
@@ -56,4 +56,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+echo "ro.pac.device=$CM_DEVICE"
+
 echo "# end build properties"

PAC-ROM/android_build

tools/buildinfo.sh
@@ -56,4 +56,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+echo "ro.pac.device=$PAC_DEVICE"
+
 echo "# end build properties"
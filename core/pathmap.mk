ff9108373e9a070f706aba5bb1a43782c632c2e4
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-01-03 21:55:08 -0500

    Revert "build: Use project pathmap for recovery"
    This reverts commit 9b92363d251da8f2f4dc4da2e450c514ddb02b19.
    
    Change-Id: I2db00e331a413162144be2d1390d51f00348bbda

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -41,6 +41,7 @@ pathmap_INCL := \
     libstdc++:bionic/libstdc++/include \
     mkbootimg:system/core/mkbootimg \
     opengl-tests-includes:frameworks/native/opengl/tests/include \
+    recovery:bootable/recovery \
     system-core:system/core/include \
     audio:system/media/audio/include \
     audio-effects:system/media/audio_effects/include \
